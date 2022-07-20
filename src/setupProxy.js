bb5ebe48d03b323a3d4e1e40f4db6367531af05b
OriyaHajbi <64365809+OriyaHajbi@users.noreply.github.com>
2022-07-20 16:00:25 +0300

    Add ProxyServer

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,5 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
      
     module.exports = function(app) {
         app.use(createProxyMiddleware('/api/**', { target: 'http://localhost:4000' }));
-        app.use(createProxyMiddleware('/users/**', { target: 'http://localhost:4000' }));
-        app.use(createProxyMiddleware('/lands/**', { target: 'http://localhost:4000' }));
-        app.use(createProxyMiddleware('/**', { target: 'http://localhost:4000' }));
+        app.use(createProxyMiddleware('/otherApi/**', { target: 'http://localhost:4000' }));
     };
